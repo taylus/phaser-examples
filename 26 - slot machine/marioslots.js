19c23a4f135fdbe57f24a770208e962bcd01ee2a
Taylus <dragondude@gmail.com>
2015-09-11 11:13:22 -0500

    Math.sign shim

--- 26 - slot machine/marioslots.js
+++ 26 - slot machine/marioslots.js
@@ -97,4 +97,10 @@
         }
         return array[closestIndex];
     }
+    
+    Math.sign = Math.sign || function(x) {
+        x = +x; // convert to a number
+        if (x === 0 || isNaN(x)) return x;
+        return x > 0 ? 1 : -1;
+    };
 //}());
